966a66382a1d446effb1c2f17255543ec4ce6c78
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2021-08-28 16:37:32 +0200

    geteri, seteri, kontruktor restorana

--- src/beans/Restaurant.java
+++ src/beans/Restaurant.java
@@ -9,7 +9,66 @@ public class Restaurant {
 	private ArrayList<Article> articles;
 	private RestaurantStatus status;
 	private Location location;
-	//logo restorana
+	private String logo;
 
 	public Restaurant() {}
+
+	public Restaurant(String restaurantName, RestaurantType restaurantType, ArrayList<Article> articles,
+			RestaurantStatus status, Location location, String logo) {
+		super();
+		this.restaurantName = restaurantName;
+		this.restaurantType = restaurantType;
+		this.articles = articles;
+		this.status = status;
+		this.location = location;
+		this.logo = logo;
+	}
+
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+
+	public RestaurantType getRestaurantType() {
+		return restaurantType;
+	}
+
+	public void setRestaurantType(RestaurantType restaurantType) {
+		this.restaurantType = restaurantType;
+	}
+
+	public ArrayList<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(ArrayList<Article> articles) {
+		this.articles = articles;
+	}
+
+	public RestaurantStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(RestaurantStatus status) {
+		this.status = status;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
 }
