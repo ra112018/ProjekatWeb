eda2e650cd8a663e2fdb0f83e27c2feb361436a9
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2021-09-03 15:27:54 +0200

    sitna popravka

--- static/app/selectedRestaurant.js
+++ static/app/selectedRestaurant.js
@@ -66,37 +66,7 @@ Vue.component("selected-restaurant", {
 		<a href="#" v-if="this.role==='manager'">Utisci i komentari</a>
 	 </div>
 	
-		<div class="pretraga">
-		<input type="text" placeholder="Naziv" id="naziv" name="naziv">
-		<input type="text" placeholder="Lokacija" id="lokacija" name="lokacija">
-		<input type="text" placeholder="Tip" id="tip" name="tip">
-		<input type="text" placeholder="Ocena" id="ocena" name="ocena">
-		 <input type="submit" value="Pretraži"><br><br><strong>Filtriraj: </strong>
-		 <select name="kriterijum" id="kriterijum">
-			  <option value="brza">Brza hrana</option>
-			  <option value="giros">Giros</option>
-			  <option value="kineska">Kineska hrana</option>
-			  <option value="kuvana">Kuvana jela</option>
-			  <option value="palačinke">Palačinke</option>
-			  <option value="pizza">Pizza</option>
-			  <option value="poslasticarnica">Poslastičarnica</option>
-			  <option value="susi">Suši</option>
-		</select>  
-		<input type="checkbox" id="otvoreni" name="otvoreni" value="Samo otvoreni">
-		<label for="otvoreni">Samo otvoreni   </label>
-&nbsp;
-	  	<strong>Sortiraj prema: </strong> 
-		<select name="sortiranje" id="sort">
-			  <option value="naziv">Naziv restorana</option>
-			  <option value="lokacija">Lokacija</option>
-			  <option value="ocena">Prosečna ocena</option>
-		</select>  
-		<select name="kriterijum" id="kriterijum">
-			  <option value="rastuće">Rastuće</option>
-			  <option value="opadajuće">Opadajuće</option>
-		</select>
 		
-		</div>
 	<div class="restoran">
 		<img class="logo3" src="dizni-logo.jpg"/>
 		<span class="opis"><br><br><input v-model="restaurantName"><br><input v-model="type"><p class="open">Otvoreno</p> 10:00-22:00
